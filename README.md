57de0ac1484da48a904b9522730058ba414f6202
Erik <github@rm-rf.dk>
2025-05-28 18:48:59 +0200

    IDK

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 clone the repository `git clone https://github.com/Willow0349/gog-better-discount-checker.git`  
 go into the local copy `cd gog-better-discount-checker`  
 set the `COOKIE` environment variable it starts with `gog-al=` and is about 150 charaters long (include the `gog-al=` part).  
-The top priority is adding proper login so this is only temporary.  
 execute the script `./gog-better-discount-checker.py`
 
 The script will print out a list of games with a better discount than what you paid for them.  
--- gog-better-discount-checker.py
+++ gog-better-discount-checker.py
@@ -3,75 +3,87 @@ import requests
 import json
 import re
 import os
+import sys
 
-cookie = os.environ.get('COOKIE')
+if not os.environ.get('COOKIE') ==  "":
+    cookie = os.environ.get('COOKIE')
+else:
+    print("Please set the COOKIE environment mariable to your gog-al token")
+    sys.exit(1)
 
-def _get_countrycode(cookie,):
+if cookie.startswith("gog-al=") and len(cookie) == 157:
+    pass
+elif not "=" in cookie and len(cookie) == 150:
+    cookie = "gog-al=" + cookie
+else:
+    print("Problem parsing gog-al variable ): ", cookie)
+    sys.exit(2)
+
+def _getCountryCode(cookie,):
     url = 'https://www.gog.com/csrf-cookie'
     headers = {'Cookie': cookie}
     r = requests.get(url, headers=headers)
     return r.cookies.get('gog_lc')[:2]
 
 
-def _get_page(cookie, page = 1):
+def _getPage(cookie, page):
     url = f'https://www.gog.com/account/settings/orders/data?canceled=0&completed=1&in_progress=1&not_redeemed=1&page={page}&pending=1&redeemed=1'
     headers = {'Cookie': cookie}
     r = requests.get(url, headers=headers)
     orders = json.loads(r.text)
     return orders
 
-def _check_page(page):
+def _checkPage(page):
     for order in page["orders"]:
-        if order['paymentMethod'] == 'Free Order':
-#            print ("FREE ORDER")
-             pass
-        elif order['moneybackGuarantee']:
+        if order['paymentMethod'] != 'Free Order' and order['moneybackGuarantee']:
             for product in order['products']:
                 if not product['isRefunded']:
                     id = int(product['id'])
                     paid = int(product['price']['amount'].replace('.', ''))
                     currency = product['cashValue']['code']
 
-                    if currency not in purchases_dict:
-                        purchases_dict[currency] = {}
+                    if currency not in purchasesDict:
+                        purchasesDict[currency] = {}
 
-                    purchases_dict[currency][id] = paid, product['title']
+                    purchasesDict[currency][id] = paid, product['title']
+        elif order['paymentMethod'] != 'Free Order':
+            return True
 
-def make_request(params):
-    request_url = f"{base_url}?ids={params['ids']}&currency={params['currency']}&countryCode={params['countryCode']}"
-#    print(f"Request URL: {request_url}")  # Print the request URL
-    response = requests.get(request_url)  # Use the full URL directly
+def _makeRequest(params):
+    requestUrl = f"{baseUrl}?ids={params['ids']}&currency={params['currency']}&countryCode={params['countryCode']}"
+    response = requests.get(requestUrl)
     if response.status_code == 200:
         return response.json()
     else:
         print(f"Failed to get response: {response.status_code}")
 
-base_url = 'https://api.gog.com/products/prices'
-purchases_dict = {}
-countryCode = _get_countrycode(cookie)
-pageCount = _get_page(cookie)['totalPages']
-for pageNumber in range(1, pageCount + 1):
-    page = _get_page(cookie, pageNumber)
-    _check_page(page)
+baseUrl = 'https://api.gog.com/products/prices'
+purchasesDict = {}
+countryCode = _getCountryCode(cookie)
+firstPage = _getPage(cookie, 1)
+pageCount = firstPage['totalPages']
+
+if not _checkPage(firstPage):
+    for pageNumber in range(2, pageCount + 1):
+        page = _getPage(cookie, pageNumber)
+        if _checkPage(page):
+            break
 
 # Make requests for each currency
-for currency, ids_dict in purchases_dict.items():
-    ids = list(ids_dict.keys())
+for currency, idsDict in purchasesDict.items():
+    ids = list(idsDict.keys())
     # Split the IDs into chunks of 99 because of api limits
     for i in range(0, len(ids), 99):
-        ids_chunk = ids[i:i + 99]
-        ids_str = "%2C".join(map(str, ids_chunk))
-        params = {'ids': ids_str, 'currency': currency, 'countryCode': countryCode}
-        response = make_request(params)
+        idsChunk = ids[i:i + 99]
+        idsStr = "%2C".join(map(str, idsChunk))
+        params = {'ids': idsStr, 'currency': currency, 'countryCode': countryCode}
+        response = _makeRequest(params)
 
         for item in response['_embedded']['items']:
             id = item['_embedded']['product']['id']
             currentPrice = int(re.sub(r'\D', '', item['_embedded']['prices'][0]['finalPrice']))
             try:
-                if purchases_dict[currency][id][0] <= currentPrice:
-#                    print ("No Problems: ", id)
-                    pass
-                else:
-                    print ('\nGOOD NEWS!', purchases_dict[currency][id][1], 'is cheaper\n')
+                if purchasesDict[currency][id][0] > currentPrice:
+                    print ('\nGOOD NEWS!', purchasesDict[currency][id][1], 'is cheaper :)\n')
             except:
                 print ("Error! Product id: ", id)
